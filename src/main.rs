4d9063032b2dc4b70c21ed3b94324bce1d04f047
Yi Sun <21307130094@m.fudan.edu.cn>
2024-12-13 07:57:26 +0800

    test: add the dfs test case

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,33 @@ mod ast;
 lalrpop_mod!(pub teapl);
 
 use lalrpop_util::lalrpop_mod;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
+    let path = "tests/progs/dfs.tea";
+    let mut file = File::open(path).unwrap();
+    let mut prog = String::new();
+    file.read_to_string(&mut prog).unwrap();
+    let ast = teapl::ProgramParser::new().parse(&prog).unwrap();
+    println!("{:#?}", ast);
+
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::Read;
+    lalrpop_mod!(pub teapl);
+    use lalrpop_util::lalrpop_mod;
+
+    #[test]
+    fn test_dfs() {
+        let path = "tests/progs/dfs.tea";
+        let mut file = File::open(path).unwrap();
+        let mut prog = String::new();
+        file.read_to_string(&mut prog).unwrap();
+        assert!(teapl::ProgramParser::new().parse(&prog).is_ok());
+    }
 }
